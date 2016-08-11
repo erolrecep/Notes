08b6ff2fe9e7035bcd7236d4473a04641117c108
Recep Erol <airforce@airforce.local>
2016-08-11 11:08:52 -0500

    python installation script added

---
+++ mac_python_installation.sh
@@ -0,0 +1,18 @@
+brew update
+brew upgrade
+
+brew install python
+brew install git
+
+brew install wget
+wget https://bootstrap.pypa.io/get-pip.py
+
+python get-pip.py
+
+echo "Pip list"
+pip list
+
+pip install jupyter
+pip install numpy scipy pandas
+pip install virtualenv virtualenvwrapper
+pip install newspaper
